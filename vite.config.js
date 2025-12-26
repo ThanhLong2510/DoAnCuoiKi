91dcda0167bebd9129e65489e16a4d90d4763cff
ThanhLong2510 <lvtlong25@gmail.com>
2025-12-26 18:08:22 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// ⚠️ Đổi <tên-repo> thành đúng tên repo của bạn
 export default defineConfig({
   plugins: [react()],
-  base: "/DoAnCuoiKi/",
 });
