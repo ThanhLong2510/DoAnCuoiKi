452bcfc032a33d0769580908c4504ae40a5e8190
ThanhLong2510 <lvtlong25@gmail.com>
2025-12-26 17:24:06 +0700

    Ready for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+// ⚠️ Đổi <tên-repo> thành đúng tên repo của bạn
 export default defineConfig({
   plugins: [react()],
-})
-
-
+  base: "/DoAnCuoiKi/",
+});
